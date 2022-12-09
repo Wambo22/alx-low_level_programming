c4ea336dc378e45484f195760b11e9d24ba6d1a5
Wambo22 <muitiririmaureen@gmail.com>
2022-12-09 15:10:16 -0800

    Lowercase alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123)
+	{
+	if (i != 101 && i != 113)
+	{
+	putchar(i);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
