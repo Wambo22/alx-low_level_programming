52a1ddaa1fd1c08d44f7c8d427b968e3906ac19b
Wambo22 <muitiririmaureen@gmail.com>
2022-12-09 14:14:54 -0800

    Print Alphabetic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	putchar(i);
+	}
+	return (0);
+}
