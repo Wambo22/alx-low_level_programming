f2da175bc30926085405e597cb65ef9df23532de
Wambo22 <muitiririmaureen@gmail.com>
2022-12-09 15:52:30 -0800

    Lowercase alphabet in Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
