068d7357887fdf9753d0f5e68d05b3b7ff1eb67e
Wambo22 <muitiririmaureen@gmail.com>
2022-12-09 14:53:21 -0800

    Print lowercase and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+	putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
