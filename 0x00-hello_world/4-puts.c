911ebcc9d3da87bd560b33bf97cc5dc18ffea7ce
Wambo22 <muitiririmaureen@gmail.com>
2022-12-11 22:26:05 -0800

    use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main(void) 
-{ 
-puts("\"Programming is like building a multilingual puzzle");
- 	return (0);
-}	
+int main(void)
+{
+
+    puts("\"programming is like building a multilingual puzzle");
+    return (0);
+}
