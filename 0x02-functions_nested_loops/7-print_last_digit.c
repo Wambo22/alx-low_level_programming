bdd0b4ef4fe45302efa15f8099d637bccda0876d
Wambo22 <muitiririmaureen@gmail.com>
2022-12-14 14:26:58 -0800

    0-putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,19 @@
-#include 'main.h'
+#include "main.h"
 /**
  * main - Entry point
  * Description: prints the last number of a digit
  *
  * Return the value of last digit
+ *
  */
-void int print_last_digit(void)
+void print_last_digit(int)
 {
-int ld = n % 10;
+int n;
+int ld;
 
+ld = n % 10;
 if (ld < 0)
 	ld * = -1;
 
 _putchar (ld + '0');
-
-return (0);
 }
