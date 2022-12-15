9c75bde2d934f203de14b234b879710da738bcbd
Wambo22 <muitiririmaureen@gmail.com>
2022-12-15 09:10:55 -0800

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,16 +3,8 @@
 
 int _isupper(int c);                                                                                                                            
 int _isdigit(int c);                                                                                                                             
-int mul(int a, int b);                                                                                                                         
-int _islower(int c);                                                                                                                                   
-int _isalpha(int c);                                                                                                                                   
-int print_sign(int n);                                                                                                                                 
-int _abs(int);                                                                                                                                         
-int print_last_digit(int);                                                                                                                             
-void jack_bauer(void);                                                                                                                                 
-void times_table(void);                                                                                                                                
-int add(int, int);                                                                                                                                     
-void print_to_98(int n);
+int mul(int a, int b);
+void print_numbers(void);
 
 #endif
 
