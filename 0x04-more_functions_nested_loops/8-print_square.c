37808d340ac12858005446881040b3fd802b3e3e
Wambo22 <muitiririmaureen@gmail.com>
2022-12-15 18:31:20 -0800

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -prints a square.
+ * @size: square size
+ *
+ * Return: noyhing on success
+ */
+void print_square(int size)
+{
+int count = 0, start;
+
+if (size <= 0)
+putchar('\n');
+else
+{
+for (start = 0; start < size; start++)
+{
+while (count < size)
+{
+putchar('#');
+count++;
+}
+count = 0;
+putchar ('\n');
+}
+}
+}
