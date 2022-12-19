43fc850284ec137eb1750f56f2771779d3ef4abe
Wambo22 <muitiririmaureen@gmail.com>
2022-12-19 03:11:30 -0800

    pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - funtion that takes a pointer to an interger as parameter
+ * @n: interger pointer
+ * Return:0 success
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
